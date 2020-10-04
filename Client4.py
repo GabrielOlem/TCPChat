091616bf598093e2aea7824cb680fda500ab7cb8
GabrielOlem <gme@cin.ufpe.br>
2020-10-04 00:08:22 -0300

    Passando mais info

--- Client4.py
+++ Client4.py
@@ -4,23 +4,25 @@ import pickle
 
 
 name = input('Insert your name:')
-HOST = '179.106.3.142'
-PORT = 5555            # Porta que o Servidor esta
+HOST = socket.gethostname()
+PORT = 22222            # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
+tcp.send(name.encode())
 
 
 def escuta():
     while 1:
         result = tcp.recv(2048)
-        print (result)
+        result = pickle.loads(result)
+        print (result[0][0], ':', result[0][1], '/~', result[1].decode())
 
 
 _thread.start_new_thread(escuta, ())
 msg = 'a'
 while msg != '\x18':
-    msg = input(HOST + ':' + 'porta' + '/~' + name + ': ')
+    msg = input()
     tcp.send (msg.encode())
 
     
--- Server4.py
+++ Server4.py
@@ -3,20 +3,22 @@ import _thread
 import pickle
 
 host = ''
-port = 5555
+port = 22222
+amount = 0
 usuarios = []
 
-def conectado(con, cliente):
+def conectado(user):
     print ('Conectado por', cliente)
     while 1:
-        msg = con.recv(2048)
+        msg = user[0].recv(2048)
         if not msg: break
-        print (cliente, msg)
         for x in usuarios:
-            if x != con:
+            if x != user:
                 #a = (cliente[0], cliente[1], msg)
                 #teste = pickle.dumps(a)
-                x.send(msg)
+                x[0].send(pickle.dumps((cliente, msg)))
+                print('Message sent by', x[2][0])
+            
     print ('Finalizando conexao do cliente', cliente)
     #usuarios.remove(con)
     con.close()
@@ -31,8 +33,10 @@ tcp.listen(1)
 
 while 1:
     con, cliente = tcp.accept()
+    recebe = con.recv(2048)
     #con.send(pickle.dumps((cliente[0], cliente[1], )))
-    usuarios.append(con)
-    _thread.start_new_thread(conectado, tuple([con, cliente]))
+    usuarios.append((con, recebe, cliente))
+    _thread.start_new_thread(conectado, tuple([usuarios[amount]]))
+    amount += 1
 
 tcp.close()
